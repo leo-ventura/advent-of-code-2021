c82c50740a242a4412a9f651c89a6004bf7ca1c1
Leonardo Ventura <leonardo.venturacl@gmail.com>
2021-12-03 02:47:09 -0300

    feat: add utility to read strings from file

--- pkg/util/filereader.go
+++ pkg/util/filereader.go
@@ -13,6 +13,27 @@ func Must(err error) {
 	}
 }
 
+// Read strings from `filename`
+func ReadStringsFromFile(filename string) []string {
+	file, err := os.Open(filename)
+	Must(err)
+
+	defer func() {
+		err = file.Close()
+		Must(err)
+	}()
+
+	scanner := bufio.NewScanner(file)
+
+	var input []string
+	for scanner.Scan() { // internally, it advances token based on sperator
+		line := scanner.Text()
+		input = append(input, line)
+	}
+
+	return input
+}
+
 // Read list of integers from file specified by `filename`
 func ReadIntegersFromFile(filename string) []int {
 	file, err := os.Open(filename)
@@ -31,7 +52,6 @@ func ReadIntegersFromFile(filename string) []int {
 		integerInt, err := strconv.Atoi(integerString)
 		Must(err)
 		integersInput = append(integersInput, integerInt)
-		Must(err)
 	}
 
 	return integersInput
